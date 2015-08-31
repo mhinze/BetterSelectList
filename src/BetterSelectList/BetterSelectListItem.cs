b8075582f55498d01c8cc9cbd186e70977ce4637
Matt Hinze <mhinze@gmail.com>
2015-08-30 19:04:03 -0500

    made Group a property to match the base's convention and slight reformatting

--- src/BetterSelectList/BetterSelectListItem.cs
+++ src/BetterSelectList/BetterSelectListItem.cs
@@ -6,26 +6,33 @@ namespace BetterSelectList
     // ReSharper disable MemberCanBePrivate.Global
     public class BetterSelectListItem : SelectListItem
     {
-
-		public string Group = null;
-
-        public BetterSelectListItem() { }
+        public BetterSelectListItem()
+        {
+        }
 
         public BetterSelectListItem(string value,
-                                    string text,
-                                    bool selected = false,
-                                    IDictionary<string, object> htmlAttributes = null,
-									string group = null)
+            string text,
+            bool selected = false,
+            IDictionary<string, object> htmlAttributes = null,
+            string group = null)
         {
             Value = value;
             Text = text;
             Selected = selected;
             HtmlAttributes = htmlAttributes;
-			Group = group;
+            Group = group;
         }
 
-        public BetterSelectListItem(string value, string text, bool selected = false, object htmlAttributes = null, string group = null)
-            : this(value, text, selected, HtmlHelper.AnonymousObjectToHtmlAttributes(htmlAttributes), group) { }
+        public BetterSelectListItem(string value,
+            string text,
+            bool selected = false,
+            object htmlAttributes = null,
+            string group = null)
+            : this(value, text, selected, HtmlHelper.AnonymousObjectToHtmlAttributes(htmlAttributes), group)
+        {
+        }
+
+        public string Group { get; set; }
 
         public IDictionary<string, object> HtmlAttributes { get; set; }
     }
